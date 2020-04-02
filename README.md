b4147e8eed307a19d3ea10362499dc8b017709e3
CircuitBreaker88 <sarris88p@gmail.com>
2020-04-02 17:34:43 -0400

    MoneyByte Changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bitcore-build-innova
+# bitcore-build
 
 A helper to add tasks to gulp.
 
@@ -7,14 +7,14 @@ A helper to add tasks to gulp.
 Install with:
 
 ```sh
-npm install bitcore-build-innova
+npm install bitcore-build
 ```
 
 and use and require in your gulp file: 
 
 ```javascript
 var gulp = require('gulp');
-var bitcoreTasks = require('bitcore-build-innova');
+var bitcoreTasks = require('bitcore-build');
 
 bitcoreTasks('submodule');
 gulp.task('default', ['lint', 'test', 'browser', 'coverage']);
@@ -25,7 +25,7 @@ gulp.task('default', ['lint', 'test', 'browser', 'coverage']);
 * There's no default task to allow for each submodule to set up their own configuration
 * If the module is node-only, avoid adding the browser tasks with:
 ```javascript
-var bitcoreTasks = require('bitcore-build-innova');
+var bitcoreTasks = require('bitcore-build');
 bitcoreTasks('submodule', {skipBrowsers: true});
 ```
 
--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function startGulp(name, opts) {
   var tests = ['test/**/*.js'];
   var alljs = files.concat(tests);
 
-  var buildPath = './node_modules/bitcore-build-innova/';
+  var buildPath = './node_modules/bitcore-build/';
   var buildModulesPath = buildPath + 'node_modules/';
   var buildBinPath = buildPath + 'node_modules/.bin/';
 
@@ -129,9 +129,9 @@ function startGulp(name, opts) {
     var browserifyCommand;
 
     if (name !== 'lib') {
-      browserifyCommand = browserifyPath + ' --require ./index.js:' + fullname + ' --external bitcore-lib-innova -o ' + fullname + '.js';
+      browserifyCommand = browserifyPath + ' --require ./index.js:' + fullname + ' --external bitcore-lib -o ' + fullname + '.js';
     } else {
-      browserifyCommand = browserifyPath + ' --require ./index.js:bitcore-lib-innova -o bitcore-lib-innova.js';
+      browserifyCommand = browserifyPath + ' --require ./index.js:bitcore-lib -o bitcore-lib.js';
     }
 
     gulp.task('browser:uncompressed', shell.task([
